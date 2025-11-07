4dc4e9bbf1e5a2889b15cdb16b644a085c2d50b4
Ibraheem Shahzad <132009830+IbraheemShahzad@users.noreply.github.com>
2025-11-07 09:51:32 +0500

    Update database.py

--- autism-spetrum-main/config/database.py
+++ autism-spetrum-main/config/database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-mongoDbUrl="mongodb+srv://gokuhulk234:a4sivdEq0gCifmnE@cluster0.y5w9ppw.mongodb.net/autism_spectrum?retryWrites=true&w=majority"
+mongoDbUrl=""
 port=8000
 client = MongoClient(mongoDbUrl, port)
 db = client["autism_spectrum"]
